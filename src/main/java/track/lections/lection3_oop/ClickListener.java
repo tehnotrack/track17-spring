bebd916b44b33f60b3507df9ceb8a1ff6ebf409c
DmitryArkhangelskiy <dmitry.arkhangelskiy@odnoklassniki.ru>
2017-03-05 13:05:27 +0300

    l3

---
+++ src/main/java/track/lections/lection3_oop/ClickListener.java
@@ -0,0 +1,15 @@
+package track.lections.lection3_oop;
+
+/**
+ *
+ */
+public interface ClickListener {
+
+    int CONST = 10;
+    // do smth on click
+    void  onClick();
+
+    default int foo() {
+        return 10;
+    }
+}
