1ee65479f79f9133a246eeaaefba109c6d9e515c
Sabelnikova Alexandra <asabelnikova@gmail.com>
2017-04-04 16:17:21 +0300

    Add files via upload

---
+++ src/main/java/track/lections/NIOExamples.java
@@ -0,0 +1,56 @@
+package com.company;
+
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NIOExamples {
+    public static void main(String[] args) {
+
+    }
+
+    public void BasicChannel() throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile("data/nio-data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf);
+        while (bytesRead != -1) {
+
+            System.out.println("Read " + bytesRead);
+            buf.flip();
+
+            while(buf.hasRemaining()){
+                System.out.print((char) buf.get());
+            }
+
+            buf.clear();
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+
+    public void BasicBufferUsage() throws IOException {
+        RandomAccessFile aFile = new RandomAccessFile("data/nio-data.txt", "rw");
+        FileChannel inChannel = aFile.getChannel();
+
+//create buffer with capacity of 48 bytes
+        ByteBuffer buf = ByteBuffer.allocate(48);
+
+        int bytesRead = inChannel.read(buf); //read into buffer.
+        while (bytesRead != -1) {
+
+            buf.flip();  //make buffer ready for read
+
+            while(buf.hasRemaining()){
+                System.out.print((char) buf.get()); // read 1 byte at a time
+            }
+
+            buf.clear(); //make buffer ready for writing
+            bytesRead = inChannel.read(buf);
+        }
+        aFile.close();
+    }
+}
